a6e9647ae8f8aafec3ef542fc20afa96ce06b8ce
Philip Borgström <philip.borgstrom@gmail.com>
2022-12-03 17:35:52 +0100

    Add simple uncontrollable player (#5)

--- Scripts/Actor/Player.cs
+++ Scripts/Actor/Player.cs
@@ -8,19 +8,19 @@ public class Player : RigidBody2D
 
 	public override void _Process(float delta)
 	{
-		ApplyThrust();
+		ApplyThrust(delta);
 	}
 
-	private void ApplyThrust()
+	private void ApplyThrust(float delta)
 	{
 		var force = 
 			(Vector2.Up * Input.GetActionStrength("player_up"))
-			* (Vector2.Left * Input.GetActionStrength("player_left") * 0.15f)
-			* (Vector2.Right * Input.GetActionStrength("player_right") * 0.15f)
-			* (Vector2.Down * Input.GetActionStrength("player_down") * 0.1f);
-		AddCentralForce(force);
+			+ (Vector2.Left * Input.GetActionStrength("player_left") * 0.25f)
+			+ (Vector2.Right * Input.GetActionStrength("player_right") * 0.25f)
+			+ (Vector2.Down * Input.GetActionStrength("player_down") * 0.25f);
+		AddCentralForce(force * delta * 100);
 
 		var torque = Input.GetActionStrength("player_rot_right") - Input.GetActionStrength("player_rot_left");
-		AddTorque(torque);
+		AddTorque(torque * delta * 100);
 	}
 }
