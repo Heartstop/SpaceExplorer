15ea742ed6b0b8730bbd571aa5882bed7227992e
Philip Borgström <philip.borgstrom@gmail.com>
2022-12-03 18:06:21 +0100

    Make player control simpler, with physics bug fix.

--- Scripts/Actor/Player.cs
+++ Scripts/Actor/Player.cs
@@ -2,25 +2,20 @@ using Godot;
 
 public class Player : RigidBody2D
 {
-	public override void _Ready()
-	{
-	}
+	public float RotSpeed => 2;
+	public float ThrustSpeed => 100;
 
-	public override void _Process(float delta)
+	public override void _PhysicsProcess(float delta)
 	{
 		ApplyThrust(delta);
 	}
 
 	private void ApplyThrust(float delta)
 	{
-		var force = 
-			(Vector2.Up * Input.GetActionStrength("player_up"))
-			+ (Vector2.Left * Input.GetActionStrength("player_left") * 0.25f)
-			+ (Vector2.Right * Input.GetActionStrength("player_right") * 0.25f)
-			+ (Vector2.Down * Input.GetActionStrength("player_down") * 0.25f);
-		AddCentralForce(force * delta * 100);
+		var force = Vector2.Up * Input.GetActionStrength("player_up");
+		LinearVelocity += delta * ThrustSpeed * force.Rotated(GlobalRotation);
 
-		var torque = Input.GetActionStrength("player_rot_right") - Input.GetActionStrength("player_rot_left");
-		AddTorque(torque * delta * 100);
+		var torque = Input.GetActionStrength("player_right") - Input.GetActionStrength("player_left");
+		AngularVelocity += delta * RotSpeed * torque;
 	}
 }
