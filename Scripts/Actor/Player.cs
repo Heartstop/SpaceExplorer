93ef87e78b1587859d3873689f33067a33fcc3ba
matte250 <matte_1500@hotmail.com>
2022-12-04 19:52:47 +0100

    Add-refueling (#33)

--- Scripts/Actor/Player.cs
+++ Scripts/Actor/Player.cs
@@ -5,11 +5,16 @@ namespace SpaceExplorer.Scripts.Actor;
 
 public class Player : RigidBody2D
 {
+	const float FUEL_CONSUMPTION_RATE = 4f;
+	const float REFUEL_RATE = 8f;
+
 	[Signal] public delegate void HealthChanged(int newHealth);
 	[Signal] public delegate void FuelChanged(int newFuel);
 
 	public float RotSpeed => 2;
 	public float ThrustSpeed => 100;
+
+	public bool IsOnLandingPad { get; private set; } = false;
 
 	[Export]
 	public float MaxHealth = 100;
@@ -20,9 +25,9 @@ public class Player : RigidBody2D
 	private AnimatedSprite _thrustAnimatedSprite = null!;
 	private AudioStreamPlayer2D _rocketAudio = null!;
 	private AudioStreamPlayer2D _impactAudio = null!;
+	private AudioStreamPlayer2D _refuelAudio = null!;
 	private AudioStreamPlayer2D _explosionAudio = null!;
 	private Particles2D	_dustParticles = null!;
-	private Particles2D _debrisParticles = null!;
 	private Sprite _shipSprite = null!;
 
 	// Vars
@@ -35,15 +40,17 @@ public class Player : RigidBody2D
 		_thrustAnimatedSprite = GetNode<AnimatedSprite>("ThrustAnimation");
 		_rocketAudio = GetNode<AudioStreamPlayer2D>("RocketAudio");
 		_impactAudio = GetNode<AudioStreamPlayer2D>("ImpactAudio");
+		_refuelAudio = GetNode<AudioStreamPlayer2D>("RefuelAudio");
 		_explosionAudio = GetNode<AudioStreamPlayer2D>("ExplosionNode/ExplosionAudio");
 		_dustParticles = GetNode<Particles2D>("ExplosionNode/DustParticles");
-		_debrisParticles = GetNode<Particles2D>("ExplosionNode/DebrisParticles");
 		_shipSprite = GetNode<Sprite>("ShipSprite");
 		_thrustAnimatedSprite.Play();
 		_health = MaxHealth;
 		_fuel = MaxFuel;
 
-		GetNode<Area2D>("FeetHurtBox").Connect("body_entered", this, nameof(OnBodyShapeEnteredFeet));
+		var feetHurtBox = GetNode<Area2D>("FeetHurtBox");
+		feetHurtBox.Connect("body_entered", this, nameof(OnBodyShapeEnteredFeet));
+		feetHurtBox.Connect("body_exited", this, nameof(OnBodyShapeExitedFeet));
 		GetNode<Area2D>("HullHurtBox").Connect("body_entered", this, nameof(OnBodyShapeEnteredHull));
 		_impactAudio.Connect("finished", this, nameof(OnImpactSoundFinished));
 	}
@@ -62,12 +69,30 @@ public class Player : RigidBody2D
 			_rocketAudio.Stop();
 		}
 
-		ConsumeFuel(); 
+		ConsumeFuel(delta);
+		Refueling(delta);
 	}
 
-	public void ConsumeFuel() {
+	public void Refueling(float delta){
+		if(!IsOnLandingPad || _fuel >= MaxFuel || Input.IsActionPressed("player_up"))
+		{
+			if(_refuelAudio.Playing)
+				_refuelAudio.Stop();
+
+			return;
+		}
+
+		if(!_refuelAudio.Playing)
+			_refuelAudio.Play();
+
+		_refuelAudio.PitchScale = (_fuel / MaxFuel) * 0.25f + 0.75f;
+		_fuel += delta * REFUEL_RATE;
+		EmitSignal(nameof(FuelChanged), _fuel);
+	}
+
+	public void ConsumeFuel(float delta) {
 		var inputStrength = Input.GetActionStrength("player_up");
-		_fuel -= inputStrength * 0.05f;
+		_fuel -= inputStrength * delta * FUEL_CONSUMPTION_RATE;
 		EmitSignal(nameof(FuelChanged), _fuel);
 	}
 
@@ -89,7 +114,18 @@ public class Player : RigidBody2D
 		AngularVelocity += delta * RotSpeed * torque;
 	}
 
-	private void OnBodyShapeEnteredFeet(Node _) => DamageShip(0.1f);
+	private void OnBodyShapeEnteredFeet(Node node){ 
+		if(node is LandingPlatform)
+			IsOnLandingPad = true;
+
+		DamageShip(0.1f);	
+	}
+
+	private void OnBodyShapeExitedFeet(Node node){ 
+		if(node is LandingPlatform)
+			IsOnLandingPad = false;
+	}
+
 	private void OnBodyShapeEnteredHull(Node _) => DamageShip(0.5f);
 
 	private void DamageShip(float modifier = 1f)
@@ -117,7 +153,6 @@ public class Player : RigidBody2D
 	private void Explode()
 	{
 		_dustParticles.Emitting = true;
-		_debrisParticles.Emitting = true;
 		_explosionAudio.Play();
 		_shipSprite.Hide();
 		Mode = RigidBody2D.ModeEnum.Static;
---
+++ Scripts/LandingPlatform.cs
@@ -0,0 +1,6 @@
+using Godot;
+
+public class LandingPlatform : StaticBody2D
+{
+
+}
