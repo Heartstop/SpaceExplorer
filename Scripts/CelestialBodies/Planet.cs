34425160eddc7df97ba8246ef3d7b5536c33a001
matte250 <matte_1500@hotmail.com>
2022-12-04 18:33:28 +0100

    Export gravity factor (#31)

--- Scripts/CelestialBodies/Planet.cs
+++ Scripts/CelestialBodies/Planet.cs
@@ -4,8 +4,9 @@ namespace SpaceExplorer.Scripts.CelestialBodies;
 
 public class Planet : CelestialBody
 {
+	[Export] float GravityFactor = 4;
 	public override void _Ready()
 	{
-		GetNode<Area2D>("GravityField").Gravity = 2 * Mathf.Pow(Scale.Length(), 3);
+		GetNode<Area2D>("GravityField").Gravity = GravityFactor * Mathf.Pow(Scale.Length(), 3);
 	}
 }
