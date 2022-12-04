6417f3db11920b83a911877702d8ed03e8f3bc2c
matte250 <matte_1500@hotmail.com>
2022-12-04 14:01:01 +0100

    Health (#23)
    * Add ui
    
    * Add ui
    
    * Remove extra player

--- Scripts/Actor/Player.cs
+++ Scripts/Actor/Player.cs
@@ -1,27 +1,45 @@
+using System;
 using Godot;
 
 namespace SpaceExplorer.Scripts.Actor;
 
 public class Player : RigidBody2D
 {
+	[Signal] public delegate void HealthChanged(int newHealth);
+
 	public float RotSpeed => 2;
 	public float ThrustSpeed => 100;
 
+	[Export]
+	public int StartHealth = 100;
+
+	// Node refs
 	private AnimatedSprite _thrustAnimatedSprite = null!;
 	private AudioStreamPlayer2D _rocketAudio = null!;
+	private AudioStreamPlayer2D _impactAudio = null!;
 	private AudioStreamPlayer2D _explosionAudio = null!;
 	private Particles2D	_dustParticles = null!;
 	private Particles2D _debrisParticles = null!;
+	private Sprite _shipSprite = null!;
+
+	// Vars
+	private Vector2 _velocityLastFrame = new Vector2(0,0); 
+	private int _health = 100;
 
 	public override void _Ready()
 	{
 		_thrustAnimatedSprite = GetNode<AnimatedSprite>("ThrustAnimation");
 		_rocketAudio = GetNode<AudioStreamPlayer2D>("RocketAudio");
+		_impactAudio = GetNode<AudioStreamPlayer2D>("ImpactAudio");
 		_explosionAudio = GetNode<AudioStreamPlayer2D>("ExplosionNode/ExplosionAudio");
 		_dustParticles = GetNode<Particles2D>("ExplosionNode/DustParticles");
 		_debrisParticles = GetNode<Particles2D>("ExplosionNode/DebrisParticles");
-
+		_shipSprite = GetNode<Sprite>("ShipSprite");
 		_thrustAnimatedSprite.Play();
+		_health = StartHealth;
+
+		Connect("body_entered", this, nameof(_on_Player_body_entered));
+		_impactAudio.Connect("finished", this, nameof(OnImpactSoundFinished));
 	}
 
 	public override void _Process(float delta)
@@ -30,7 +48,6 @@ public class Player : RigidBody2D
 		{
 			_thrustAnimatedSprite.Show();
 			_rocketAudio.Play();
-			Explode();
 		}
 
 		if (Input.IsActionJustReleased("player_up"))
@@ -43,6 +60,7 @@ public class Player : RigidBody2D
 	public override void _PhysicsProcess(float delta)
 	{
 		ApplyThrust(delta);
+		_velocityLastFrame = LinearVelocity;
 	}
 
 	private void ApplyThrust(float delta)
@@ -54,10 +72,34 @@ public class Player : RigidBody2D
 		AngularVelocity += delta * RotSpeed * torque;
 	}
 
+	private void _on_Player_body_entered(Node body)
+	{
+		var impact = (int)Math.Round(_velocityLastFrame.Length() * 0.5);
+		if(impact < 10)
+			return;
+
+		_health -= impact;
+		EmitSignal(nameof(HealthChanged), _health);
+		if(_health <= 0)
+			Explode();
+		else
+			PlayImpactSound();
+	}
+
+	private void PlayImpactSound(){
+		var pitchScale = _impactAudio.PitchScale;
+		_impactAudio.PitchScale = pitchScale + ((GD.Randf() - 0.5f) * .8f);
+		_impactAudio.Play();
+	}
+
+	private void OnImpactSoundFinished() => _impactAudio.PitchScale = 1;
+
 	private void Explode()
 	{
 		_dustParticles.Emitting = true;
 		_debrisParticles.Emitting = true;
 		_explosionAudio.Play();
+		_shipSprite.Hide();
+		Mode = RigidBody2D.ModeEnum.Static;
 	}
 }
