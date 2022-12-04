3cee47b07796bb32487b223f9b62c2639797fa8d
Mattias S <matte_1500@hotmail.com>
2022-12-04 22:56:48 +0100

    add pickup sound

--- Scripts/Minable/Minable.cs
+++ Scripts/Minable/Minable.cs
@@ -2,6 +2,15 @@ using Godot;
 
 namespace SpaceExplorer.Scripts.Minable;
 
+[Tool]
 public class Minable : RigidBody2D
 {
+    [Export] public int SpriteIndex = 0;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        GetNode<AnimatedSprite>("AnimatedSprite").Frame = SpriteIndex;
+    }
+
 }
