874ce9f27bf5ce4f3564a010a3b3040eb5002529
matte250 <matte_1500@hotmail.com>
2022-12-08 01:14:49 +0100

    Parameterize planet shader (#58)

--- Scripts/CelestialBodies/Planet.cs
+++ Scripts/CelestialBodies/Planet.cs
@@ -2,11 +2,45 @@ using Godot;
 
 namespace SpaceExplorer.Scripts.CelestialBodies;
 
+[Tool]
 public class Planet : CelestialBody
 {
 	[Export] float GravityFactor = 4;
+	[Export] Color Color1 = new Color() {};
+
+	[Export] Color Color2 = new Color();
+
+	[Export] Color OutlineColor1 = new Color();
+
+	[Export] Color OutlineColor2 = new Color();
+
+	[Export] float BorderSize = 100;
+
+	private ColorRect _colorRect = null!;
+
 	public override void _Ready()
 	{
 		GetNode<Area2D>("GravityField").Gravity = GravityFactor * Mathf.Pow(Scale.Length(), 3);
+		_colorRect = GetNode<ColorRect>("ColorRect");
+		SetShaderParams();
+	}
+
+	public override void _Process(float delta)
+	{
+		if(!Engine.EditorHint)
+			return;
+
+		SetShaderParams();
+	}
+
+	private void SetShaderParams() {
+		var material = (ShaderMaterial)_colorRect.Material;
+		material.SetShaderParam("PIXEL_SCALE", _colorRect.RectSize.x * Scale.x);
+		material.SetShaderParam("COLOR_1", Color1);
+		material.SetShaderParam("COLOR_2", Color2);
+		material.SetShaderParam("OUTLINE_COLOR_1", OutlineColor1);
+		material.SetShaderParam("OUTLINE_COLOR_2", OutlineColor2);
+		material.SetShaderParam("BORDER_SIZE", BorderSize);
+
 	}
 }
